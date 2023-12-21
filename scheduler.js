c50724e1cc0d055628b4f62a9d6d34d888bdeefb
devMrlsagency <122008760+devMrlsagency@users.noreply.github.com>
2023-12-21 03:05:58 +0100

    Delete scheduler.js

--- scheduler.js
+++
@@ -1,7 +0,0 @@
-const cron = require('node-cron');
-const { main } = require('./scrapping');
-
-cron.schedule('*/3 * * * *', () => {
-    console.log('Exécution du scraping toutes les 3 minutes');
-    main();
-});
