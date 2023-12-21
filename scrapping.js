ac0dfe9e229eee671a143ab61715a4eaed13b685
devMrlsagency <122008760+devMrlsagency@users.noreply.github.com>
2023-12-21 13:43:28 +0100

    Update scrapping.js

--- scrapping.js
+++ scrapping.js
@@ -20,14 +20,12 @@ async function fetchAnnonces() {
 
     const pages = await Promise.all([
         browser.newPage(),
-        browser.newPage(),
-        browser.newPage(),
+        
     ]);
 
     const pageUrls = [
-        `https://www.immobilier.notaires.fr/fr/annonces-immobilieres-liste?page=1&parPage=100&departement=73&typeTransaction=VENTE,VNI,VAE,LOCATION`,
-        `https://www.immobilier.notaires.fr/fr/annonces-immobilieres-liste?page=2&parPage=100&departement=73&typeTransaction=VENTE,VNI,VAE,LOCATION`,
-        `https://www.immobilier.notaires.fr/fr/annonces-immobilieres-liste?page=3&parPage=100&departement=73&typeTransaction=VENTE,VNI,VAE,LOCATION`,
+        `https://www.immobilier.notaires.fr/fr/annonces-immobilieres-liste?page=1&parPage=10&departement=73&typeTransaction=VENTE,VNI,VAE,LOCATION`,
+     
     ];
 
     const annonces = await Promise.all(
