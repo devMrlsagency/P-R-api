8043fcf2d08ceb3502db5d594cd1b4e9e8468661
devMrlsagency <122008760+devMrlsagency@users.noreply.github.com>
2023-12-21 03:03:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,21 +17,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/annonces', async (req, res) => {
-    if (cacheTime && cacheTime > Date.now() - (1000 * 30)) {
-        return res.json(data);
-    }
-
-    const filePath = './annonces.json';
-    fs.readFile(filePath, 'utf-8', (err, fileData) => {
-        if (err) {
-            return res.status(500).send('Erreur lors de la lecture du fichier');
-        } else {
-            data = JSON.parse(fileData);
-            cacheTime = Date.now();
-            res.json(data);
-        }
-    });
+app.get('/', async (req, res) => {
+    await main();
+    res.send('Annonce.json a été mis à jour.');
 });
 
 app.use(middlewares.notFound);
