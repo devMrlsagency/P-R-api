0bc2d395a2eca9d6108230ea229e7e4025818a0e
devMrlsagency <122008760+devMrlsagency@users.noreply.github.com>
2023-12-21 03:12:02 +0100

    Update apsp.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    await main();
-    res.send('Annonce.json a été mis à jour.');
-});
+app.get('/annonces', async (req, res) => {
+    if (cacheTime && cacheTime > Date.now() - (1000 * 30)) {
+        return res.json(data);
+    }
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
