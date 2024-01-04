afb2f9982bb0c6125e4ea4ec0788148afa0b2497
devMrlsagency <122008760+devMrlsagency@users.noreply.github.com>
2024-01-04 13:23:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Configuration et connexion à MongoDB avec Mongoose
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { serverSelectionTimeoutMS: 30000 })
+mongoose.connect(uri, { serverSelectionTimeoutMS: 120000 })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
